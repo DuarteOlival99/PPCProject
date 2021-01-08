b1ab88feff9efbba1bc776af7e70fbd3c7009866
Duarte Olival <duarte.frj.olival@gmail.com>
2021-01-08 20:08:25 +0000

    FORKJoin Inicializad e Finalizado

---
+++ src/ParalisedForkJoin/ForkJoinNemhauserUllman.java
@@ -0,0 +1,60 @@
+package ParalisedForkJoin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinNemhauserUllman extends RecursiveTask<List<Solution>> {
+    private static final long serialVersionUID = 1L;
+    private List<Solution> listSolutions;
+    private List<Solution> workingSolutions;
+
+    public ForkJoinNemhauserUllman(List<Solution> listSolutions, List<Solution> workingSolutions) {
+        this.listSolutions = listSolutions;
+        this.workingSolutions = workingSolutions;
+    }
+
+
+    @Override
+    protected List<Solution> compute() {
+        if (listSolutions.size() == 1) {
+
+            List<Solution> filtered = new ArrayList<>();
+            boolean nonDominated = true;
+            for (Solution sol2 : workingSolutions) {
+                if (listSolutions.get(0).isDominatedBy(sol2)) {
+                    nonDominated = false;
+                    break;
+                }
+            }
+            if (nonDominated) {
+                filtered.add(listSolutions.get(0));
+            }
+            return filtered;
+
+        }else {
+
+            //dividir a listSolutions ao meio
+
+            List<Solution> left = listSolutions.subList(0, listSolutions.size() / 2); //divide a primeira metade
+
+            List<Solution> right = listSolutions.subList(listSolutions.size() / 2, listSolutions.size()); //divide a segunda metade
+
+
+            ForkJoinNemhauserUllman f1 = new ForkJoinNemhauserUllman(left, workingSolutions);
+            f1.fork();
+
+            ForkJoinNemhauserUllman f2 = new ForkJoinNemhauserUllman(right, workingSolutions);
+            //f2.fork();
+
+            List<Solution> listRight = f2.compute();
+            List<Solution> listLeft = f1.join();
+
+            listLeft.addAll(listRight);
+
+            return listLeft;
+        }
+
+    }
+
+}
