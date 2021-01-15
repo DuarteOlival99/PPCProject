2cc7c731bf6dfdd892393520a93017bf0e2698f4
Duarte Olival <duarte.frj.olival@gmail.com>
2021-01-15 15:00:30 +0000

    media de tempos de execucao

--- src/ParalisedForkJoin/ResutTests.java
+++ src/ParalisedForkJoin/ResutTests.java
@@ -1,3 +1,5 @@
+package ParalisedForkJoin;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -9,15 +11,44 @@ public class ResutTests {
 
     public static void main(String[] args) {
 
-        HashMap<String, Integer> listSequencial = ReadFileSequencial();
-        HashMap<String, Integer> listParalisedFramework = ReadFileParalisedFramework();
-        HashMap<String, Integer> listParalisedForkJoin = ReadFileParalisedForkJoin();
+       HashMap<String, Integer> listSequencial ;
+        HashMap<String, Integer> listParalisedFramework ;
 
-        writeToFile(listSequencial, listParalisedFramework, listParalisedForkJoin);
+        File fileSequencial3 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\Sequencial\\tempo_sequencial_dim3.txt");
+        File fileSequencial4 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\Sequencial\\tempo_sequencial_dim4.txt");
+        File fileSequencial5 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\Sequencial\\tempo_sequencial_dim5.txt");
+
+        File fileSuperDuperFramework3 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedFramework\\tempo_paralised_framework.txt");
+
+        File fileForkJoin3 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_dim3.txt");
+        File fileForkJoin4 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_dim4.txt");
+        File fileForkJoin5 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_dim5.txt");
+
+        File fileSpecific3 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_Specific_dim3.txt");
+        File fileSpecific4 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_Specific_dim4.txt");
+        File fileSpecific5 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_Specific_dim5.txt");
+
+        File fileMaxLevel3 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_MaxLevel_dim3.txt");
+        File fileMaxLevel4 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_MaxLevel_dim4.txt");
+        File fileMaxLevel5 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_MaxLevel_dim5.txt");
+
+        File fileMaxTasks3 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_MaxTasks_dim3.txt");
+        File fileMaxTasks4 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_MaxTasks_dim4.txt");
+        File fileMaxTasks5 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_MaxTasks_dim5.txt");
+
+        File fileSurplus3 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_Surplus_dim3.txt");
+        File fileSurplus4 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_Surplus_dim4.txt");
+        File fileSurplus5 = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\ParalisedForkJoin\\tempo_forkJoin_Surplus_dim5.txt");
+
+
+        HashMap<String, Integer> listParalisedForkJoin = ReadFileParalisedForkJoin(fileSurplus3);
+        System.out.println("fileSurplus3");
+
+        writeToFile(listParalisedForkJoin);
 
     }
 
-    public static void writeToFile(HashMap<String, Integer> listSequencial, HashMap<String, Integer> listParalisedFramework, HashMap<String, Integer> listParalisedForkJoin){
+    public static void writeToFile(HashMap<String, Integer> listParalisedForkJoin){
         try {
 
             FileWriter myWriter = new FileWriter("tempo.txt");
@@ -29,31 +60,11 @@ public class ResutTests {
             List<Integer> valueParalisedFramework =  new ArrayList<>();
             List<Integer> valueParalisedForkJoin =  new ArrayList<>();
             Integer count = 0;
-//
-//            listParalisedForkJoin.forEach((k, v) -> {
-//                String[] parts1 = k.split("_");
-//                if (3 == Integer.parseInt(parts1[1])) { //so para a dimensao 3
-//                    if (v > maiorNumero.get()){
-//                        maiorNumero.set(v); // maior numero é = 85890
-//                    }
-//                    listFinal.add("Paralised ForkJoin;" + v + "\n");
-//                }
-//            });
 
-//            listParalisedFramework.forEach((k, v) -> {
-//                String[] parts1 = k.split("_");
-//                if (3 == Integer.parseInt(parts1[1])) { //so para a dimensao 3
-//                    if (v > maiorNumero.get()){
-//                        maiorNumero.set(v); // maior numero é = 110375
-//                    }
-//                    listFinal.add("Paralised Framework;" + v + "\n");
-//                }
-//            });
-
-            listSequencial.forEach((k, v) -> {
+            listParalisedForkJoin.forEach((k, v) -> {
 
                 String[] parts1 = k.split("_");
-                if (3 == Integer.parseInt(parts1[1])) { //so para a dimensao 3
+                //if (3 == Integer.parseInt(parts1[1])) { //so para a dimensao 3
 //                    if (v > maiorNumero.get()){
 //                        maiorNumero.set(v); // maior numero é = 527404
 //                    }
@@ -61,36 +72,24 @@ public class ResutTests {
                     //listFinal.add("Paralised Framework;" + listParalisedFramework.get(k) + "\n");
                     //listFinal.add("Paralised ForkJoin;" + listParalisedForkJoin.get(k) + "\n");
 
-                    valueSequencial.add(v);
-                    valueParalisedFramework.add(listParalisedFramework.get(k));
                     valueParalisedForkJoin.add(listParalisedForkJoin.get(k));
 
-                }
+               // }
 
-                //listFinal.add(k + " / " + v + " / " + listParalisedFramework.get(k) + " / " + listParalisedForkJoin.get(k) + "\n");
-
-//                if (v < listParalisedFramework.get(k) && v < listParalisedForkJoin.get(k)){
-//                    listFinal.add(k + " / " + v + " / " + listParalisedFramework.get(k) + " / " + listParalisedForkJoin.get(k) + "\n");
-//                }
             });
 
-            Integer finalValueSequencial = 0;
-            for (Integer value : valueSequencial){
-                finalValueSequencial += value;
-            }
-            listFinal.add("media de tempo de execucao para a versao Sequencial = " + (finalValueSequencial/valueSequencial.size())+ " Milesegundos" + "\n");
-
-            Integer finalValueParalisedFramework = 0;
-            for (Integer value : valueParalisedFramework){
-                finalValueParalisedFramework += value;
-            }
-            listFinal.add("media de tempo de execucao para a versao paralisada framework = " + (finalValueParalisedFramework/valueParalisedFramework.size())+ " Milesegundos" + "\n");
-
             Integer finalValueParalisedForkJoin = 0;
+            Integer size = 0;
             for (Integer value : valueParalisedForkJoin){
-                finalValueParalisedForkJoin += value;
+               // if (size < 466){
+                    finalValueParalisedForkJoin += value;
+                    size++;
+                //}
+
             }
-            listFinal.add("media de tempo de execucao para a versao paralisada Fork Join = " + (finalValueParalisedForkJoin/valueParalisedForkJoin.size())+ " Milesegundos" + "\n");
+            System.out.println("size = " + size);
+            System.out.println("media de tempo de execucao = " + (finalValueParalisedForkJoin/size)+ " Milesegundos" + "\n");
+            listFinal.add("media de tempo de execucao = " + (finalValueParalisedForkJoin/size)+ " Milesegundos" + "\n");
 
 
             //System.out.println("maior numero é = " + maiorNumero);
@@ -121,7 +120,7 @@ public class ResutTests {
                 String[] parts = data.split(" ");
                 list.add(Integer.valueOf(parts[2]));
                 listSequencial.put(parts[0],Integer.valueOf(parts[2]));
-                System.out.println(data);
+                //System.out.println(data);
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -129,7 +128,7 @@ public class ResutTests {
         }
 
         Collections.sort(list);
-        System.out.println(list);
+        //System.out.println(list);
         return listSequencial;
 
     }
@@ -146,7 +145,7 @@ public class ResutTests {
                 String[] parts = data.split(" ");
                 list.add(Integer.valueOf(parts[2]));
                 listParalisedFramework.put(parts[0],Integer.valueOf(parts[2]));
-                System.out.println(data);
+               // System.out.println(data);
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -154,13 +153,12 @@ public class ResutTests {
         }
 
         Collections.sort(list);
-        System.out.println(list);
+       // System.out.println(list);
         return listParalisedFramework;
 
     }
 
-    public static HashMap<String, Integer>  ReadFileParalisedForkJoin() {
-        File myObj = new File("C:\\Users\\duart\\OneDrive\\Ambiente de Trabalho\\PPCProject\\src\\tempo_paralised_forkjoin.txt");
+    public static HashMap<String, Integer>  ReadFileParalisedForkJoin(File myObj) {
         List<Integer> list = new ArrayList<Integer>();
         HashMap<String, Integer> listParalisedForkJoin = new HashMap<String,Integer>();
         Scanner myReader;
@@ -169,9 +167,10 @@ public class ResutTests {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 String[] parts = data.split(" ");
+                System.out.println("parts" + Arrays.toString(parts));
                 list.add(Integer.valueOf(parts[2]));
                 listParalisedForkJoin.put(parts[0],Integer.valueOf(parts[2]));
-                System.out.println(data);
+               // System.out.println(data);
             }
             myReader.close();
         } catch (FileNotFoundException e) {
